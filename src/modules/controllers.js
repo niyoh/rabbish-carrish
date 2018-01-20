2b145703a6c45f3f2f0403d5110f9fd1b85c7594
Martin <martinhy59@hotmail.com>
2018-01-20 17:36:24 +0800

    looks good now

--- src/modules/controllers.js
+++ src/modules/controllers.js
@@ -1,4 +1,4 @@
-import firebase, { todoRef, ref } from './firebase'
+import firebase, { todoRef, userTodoRef, ref } from './firebase'
 import express from 'express'
 
 const init = () => {
@@ -23,7 +23,12 @@ router.post('/todoItem/:userId/:todoName/:todoDeadline', (req, res) => {
     todoDeadline: todoDeadline,
     createdTime: Date.now()
   };
-  var todoId = ref.child('todos').push(todoItem).key;
+  var todoId = todoRef.push(todoItem).key;
+
+  var userTodoItem = {
+    todoId: todoId
+  };
+  userTodoRef.child(userId).push(userTodoItem);
 
   res.json({
     todoId: todoId
@@ -44,36 +49,59 @@ router.put('/todoItem/:todoId/:todoDeadline', (req, res) => {
   var todoId = req.params.todoId;
   var todoDeadline = req.params.todoDeadline;
 
-  console.log('on9');
-  ref.set("I'm writing data")
-    .then(function(){
-      console.log("Data saved successfully.");
-    })
-    .catch(function(error){
-      console.log("Data could not be saved." + error);
-    });
-
-  todoRef.child(todoId).once('value').then((todoItem) => {
+  todoRef.child(todoId).once('value').then((snapshot) => {
+    var todoItem = snapshot.val();
     todoItem.todoDeadline = todoDeadline;
 
     var update = {};
     update['/todos/' + todoId] = todoItem;
 
-    ref.update(update, function(error) {
-      console.log('damn it');
-    });
+    ref.update(update);
 
-    res.json(todoRef.child(todoId));
+    res.json(todoItem);
     res.end();
+  }).catch((error) => {
+    console.log(error);
   });
 });
 
 router.delete('/todoItem/:todoId', (req, res) => {
   var todoId = req.params.todoId;
-  console.log('fucking shit');
-  todoRef.child(todoId).remove().then(function() {
-    console.log('shit');
+
+  todoRef.child(todoId).remove().then(() => {
+    res.json({
+      result: 'Success'
+    });
+    res.end();
+  }).catch((error) => {
+    res.json({
+      result: 'Failed'
+    });
+    res.end();
   })
+});
+
+router.get('/todoItems/:userId', (req, res) => {
+  var userId = req.params.userId;
+
+  userTodoRef.child(userId).once('value', (snapshot) => {
+    var promises = [];
+    snapshot.forEach((childSnapshot) => {
+      console.log(childSnapshot.key);
+      console.log(childSnapshot.val().todoId);
+      promises.push(todoRef.child(childSnapshot.val().todoId).once('value'));
+    });
+
+    Promise.all(promises).then((snapshots) => {
+      var todoItems = [];
+      for (var i in snapshots) {
+        todoItems.push(snapshots[i].val());
+      }
+
+      res.json(todoItems);
+      res.end();
+    })
+  });
 });
 
 export default router
--- src/modules/firebase.js
+++ src/modules/firebase.js
@@ -2,11 +2,15 @@ import { EventEmitter } from 'events'
 import firebase from 'firebase'
 import path from 'path'
 
-const FIREBASE_URL = process.env.FIREBASE_URL
+const AUTH_DOMAIN = process.env.AUTH_DOMAIN
+const API_KEY = process.env.API_KEY
+const FIREBASE_URL = process.env.FIREBASE_URL || 'https://todolist-c5f4d.firebaseio.com/'
 
 if (!FIREBASE_URL) { throw new Error('FIREBASE_URL must be defined') }
 
 const config = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
   databaseURL: FIREBASE_URL,
   serviceAccount: path.join(__dirname, '../config/firebase', `service-worker-creds.${process.env.NODE_ENV === 'production' ? 'prod' : 'dev'}.json`)
 }
@@ -15,6 +19,7 @@ firebase.initializeApp(config)
 
 export const ref = firebase.database().ref()
 export const todoRef = firebase.database().ref('todos')
+export const userTodoRef = firebase.database().ref('user-todos')
 
 const firebaseModule = new EventEmitter()
 
